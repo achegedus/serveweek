0f5cea9b8863a84aaa5f51b72eb443dbadfdf498
Adam Hegedus <adam@heged.us>
2021-04-22 00:05:09 -0400

    project filters not working yet

--- app/Http/Livewire/ProjectList.php
+++ app/Http/Livewire/ProjectList.php
@@ -9,6 +9,28 @@ use Livewire\Component;
 
 class ProjectList extends Component
 {
+    public $filterRegions = [];
+    public $filterCategories = [];
+
+
+    public function toggleRegion($inRegion)
+    {
+        $key = array_search($inRegion, $this->filterRegions);
+        if ($key)
+            unset($this->filterRegions[$key]);
+        else
+            $this->filterRegions[] = $inRegion;
+    }
+
+    public function toggleCategory($inCategory)
+    {
+        $key = array_search($inCategory, $this->filterCategories);
+        if ($key)
+            unset($this->filterCategories[$key]);
+        else
+            $this->filterCategories[] = $inCategory;
+    }
+
     public function render()
     {
         return view('livewire.project-list', [
--- resources/views/livewire/project-list.blade.php
+++ resources/views/livewire/project-list.blade.php
@@ -6,19 +6,19 @@
         <div class="w-1/5 text-sm">
             <h3 class="text-xl font-bold border-b-2 border-gray-300 mb-2">Regions</h3>
             @foreach($regions as $region)
-                <div><a class="text-purple-700 underline" href="/volunteer#search?region={{$region->id}}">{{$region->name}}</a></div>
+                <div wire:key="x{{ $loop->index }}"><a class="text-purple-700 underline" wire:click.prevent="toggleRegion({{$region->id}})">{{$region->name}}</a></div>
             @endforeach
 
             <h3 class="text-xl font-bold mt-8 border-b-2 border-gray-300 mb-2">Categories</h3>
             @foreach($categories as $category)
-                <a class="text-purple-700 underline"  href="/volunteer#search?region={{$category->id}}"><div>{{$category->name}}</div></a>
+                <div wire:key="y{{ $loop->index }}"><a class="text-purple-700 underline"  wire:click.prevent="toggleCategory({{$category->id}})"><div>{{$category->name}}</div></a></div>
             @endforeach
         </div>
 
         <div class="w-4/5">
             <div class="grid md:grid-cols-2 gap-4 pl-4 pr-4">
                 @foreach($projects as $project)
-                    <livewire:project-list-item :project="$project"></livewire:project-list-item>
+                    <livewire:project-list-item :project="$project" wire:key="z{{ $loop->index }}"></livewire:project-list-item>
                 @endforeach
             </div>
         </div>
